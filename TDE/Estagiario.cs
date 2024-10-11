e21c84ae9ea24bfb16a026d0f982aff292eeb47d
Guilherme Birlem Machado <132947485+guibirlem@users.noreply.github.com>
2024-10-10 21:39:09 -0300

    TDE 10/10

---
+++ TDE/Estagiario.cs
@@ -0,0 +1,11 @@
+public class Estagiario : Funcionario 
+{
+    public override double CalcularSalario()
+    {
+        return base.CalcularSalario()/2;
+    }
+    public Estagiario(string nome, int idade): base(nome,idade)
+    {
+        Console.WriteLine("aoe");
+    }
+}
---
+++ TDE/Program.cs
@@ -0,0 +1,41 @@
+﻿// array ramazena um numero predefinido de elementos predefinido de elemtnos do mesmo tipo 
+using System.Collections;
+
+Console.WriteLine("cu");
+
+int [] numeros = new int [3];
+numeros[0] = 10;
+numeros[1] = 70;
+numeros[2] = 29;
+
+foreach (var numero in numeros)
+{
+    Console.WriteLine(numero);
+}
+//listas armazena elemntos do mesmo tipo e aumentam ou 
+//diminuim o tamanho dinamicamente 
+
+List <string> frutas = new List<string>();
+frutas.Add("maça");
+frutas.Add("Banana");
+frutas.Add("limao");
+
+frutas.Remove("maça");
+
+foreach(var fruta in frutas)
+{
+    Console.WriteLine(fruta);
+}
+
+//arraylist é uma coleção que tem tamanho dinamico 
+// e que armazena elemtnso de qualquer tipo 
+
+ArrayList itens = new  ArrayList();
+itens.Add("maça");
+itens.Add(10);
+itens.Add(20.5);
+
+foreach(var item in itens)
+{
+    Console.WriteLine(item);
+}
